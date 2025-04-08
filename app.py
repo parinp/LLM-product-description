89562483b86cb8ee60be2a017ee97b5a2e9a7774
Parin Pipake <parinpipake@gmail.com>
2025-04-08 17:11:51 +0100

    streamlit deployment

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ from templates import get_template_names, apply_template
 
 # Load environment variables
 load_dotenv()
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+# GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
 
 if not GEMINI_API_KEY:
     st.error("GEMINI_API_KEY not found. Please add it to your .env file.")
